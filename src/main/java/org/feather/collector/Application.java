1563a6bf30d7bdc654fce56941372cbcc6c71353
feather <Brian608@163.com>
2023-04-24 14:50:42 +0800

    project init

---
+++ src/main/java/org/feather/collector/Application.java
@@ -0,0 +1,20 @@
+package org.feather.collector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @projectName: collector-log
+ * @package: org.feather.collector
+ * @className: Application
+ * @author: feather(杜雪松)
+ * @description: TODO
+ * @since: 2023-04-24 14:45
+ * @version: 1.0
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
